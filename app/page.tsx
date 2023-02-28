ba20a395cc7314149d96ea55d97ec3c184dbdd1e
ljq0226 <1035821043@qq.com>
2023-02-28 23:39:40 +0800

    chore: delete login moudle

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 import Desktop from './Desktop'
-import Login from './Login'
-import { userStore } from '@/store'
 export default function Home() {
-  const username = userStore(state => state.username)
-  return <>{username ? <Desktop></Desktop> : <Login></Login>}</>
+  return <><Desktop></Desktop></>
 }
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,5 @@
-import userStore from './login'
 import themeStore from './theme'
 import useControlStore from './control'
 import useLaunchpadStore from './launchpad'
 
-export { userStore, themeStore, useControlStore, useLaunchpadStore }
+export { themeStore, useControlStore, useLaunchpadStore }
