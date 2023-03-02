b75fee06b4a8b8d1734efd62983989498ef02b2e
ljq0226 <1035821043@qq.com>
2023-03-02 11:44:30 +0800

    fix: useWindowSize:window can't be find

--- hooks/useWindowSize.ts
+++ hooks/useWindowSize.ts
@@ -2,9 +2,16 @@
 import { useEffect, useState } from 'react'
 
 export function useWindowSize() {
+  let width = 800
+  let height = 600
+  // to resolve the window Object can't be find in server side
+  if (typeof window !== 'undefined') {
+    width = window.innerWidth
+    height = window.innerHeight
+  }
   const [state, setState] = useState({
-    winWidth: window.innerWidth,
-    winHeight: window.innerHeight,
+    winWidth: width,
+    winHeight: height,
   })
 
   useEffect(() => {
