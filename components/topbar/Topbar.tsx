438e07c61c25c3004bf38eaaec63a181a58afbbc
ljq0226 <1035821043@qq.com>
2023-03-05 16:48:16 +0800

    chore: maximize windows hide topbar

--- components/topbar/Topbar.tsx
+++ components/topbar/Topbar.tsx
@@ -15,7 +15,7 @@ import WifiMenu from '../menu/WifiMenu'
 import ControlCenter from '../menu/ControlCenter/ControlCenter'
 import TopbarItem from './TopbarItem'
 import dayjs from '@/lib/day'
-import { useControlStore, useThemeStore } from '@/store'
+import { useAppsStore, useControlStore, useThemeStore } from '@/store'
 
 const Topbar = () => {
   const [dark, setDark, brightness, setBrightness, sound, setSound]
@@ -53,98 +53,104 @@ const Topbar = () => {
     ],
     shallow,
   )
-
+  const max = useAppsStore(s => s.max)
   const date = new Date()
   return (
-    <div
-      className={`w-full h-8 px-2  top-0  z-50 text-sm backdrop-blur-2xl shadow transition select-none
+    <>
+      {
+        !max && <div
+          className={`w-full h-8 px-2  top-0  z-50 text-sm backdrop-blur-2xl shadow transition select-none
        flex justify-between font-medium ${dark ? 'text-white  bg-gray-500/20 ' : 'text-black  bg-gray-700/10'
-        }`}
-    >
-      {/* Apple Icon  */}
-      <div
-        className=" flex justify-center w-[30px] items-center  relative  hover:bg-gray-400 "
-        onMouseMove={() => {
-          appleMenuSwitch(true)
-        }}
-      >
-        {/* Apple Menu Switch */}
-        <TopbarItem
-          Icon={
-            <Image
-              alt=""
-              width={300}
-              height={300}
-              src={`${dark
-                ? '/img/icons/apple-white.png'
-                : '/img/icons/apple-black.png'
-                }`}
-            />
-          }
-        ></TopbarItem>
-        {showAppleMenu && <Applemenu appleMenuSwitch={appleMenuSwitch} />}
-      </div>
-      {/* flex empty block */}
-      <div className="flex-1"></div>
-      {/* right icons list  */}
-      <div className="flex items-center justify-end h-full space-x-2 ">
-        <div className="relative">
-          {wifi
-            ? (
+            }`}
+        >
+          {/* Apple Icon  */}
+          <div
+            className=" flex justify-center w-[30px] items-center  relative  hover:bg-gray-400 "
+            onMouseMove={() => {
+              appleMenuSwitch(true)
+            }}
+          >
+            {/* Apple Menu Switch */}
+            <TopbarItem
+              Icon={
+                <Image
+                  alt=""
+                  width={300}
+                  height={300}
+                  src={`${dark
+                    ? '/img/icons/apple-white.png'
+                    : '/img/icons/apple-black.png'
+                    }`}
+                />
+              }
+            ></TopbarItem>
+            {showAppleMenu && <Applemenu appleMenuSwitch={appleMenuSwitch} />}
+          </div>
+          {/* flex empty block */}
+          <div className="flex-1"></div>
+          {/* right icons list  */}
+          <div className="flex items-center justify-end h-full space-x-2 ">
+            <div className="relative">
+              {wifi
+                ? (
+                  <TopbarItem
+                    clickHandler={wifiMenuSwitch}
+                    value={showWifiMenu}
+                    Icon={<Wifi size={16} color={dark ? '#fff' : '#000'} />}
+                  ></TopbarItem>)
+                : (
+                  <TopbarItem
+                    clickHandler={wifiMenuSwitch}
+                    value={showWifiMenu}
+                    Icon={<WifiOff size={16} color={dark ? '#fff' : '#000'} />}
+                  ></TopbarItem>)}
+              {showWifiMenu && (
+                <WifiMenu
+                  wifi={wifi}
+                  wifiSwitch={wifiSwitch}
+                  wifiMenuSwitch={wifiMenuSwitch}
+                  dark={dark}
+                />
+              )}
+            </div>
+
+            <TopbarItem
+              Icon={<Search size={16} color={dark ? '#fff' : '#000'} />}
+            ></TopbarItem>
+            <TopbarItem
+              Icon={<MoreHorizontal size={16} color={dark ? '#fff' : '#000'} />}
+            ></TopbarItem>
+            <TopbarItem
+              Icon={<BatteryCharging size={16} color={dark ? '#fff' : '#000'} />}
+            ></TopbarItem>
+            <div className="relative">
               <TopbarItem
-                clickHandler={wifiMenuSwitch}
-                value={showWifiMenu}
-                Icon={<Wifi size={16} color={dark ? '#fff' : '#000'} />}
-              ></TopbarItem>)
-            : (
-              <TopbarItem
-                clickHandler={wifiMenuSwitch}
-                value={showWifiMenu}
-                Icon={<WifiOff size={16} color={dark ? '#fff' : '#000'} />}
-              ></TopbarItem>)}
-          {showWifiMenu && (
-            <WifiMenu
-              wifi={wifi}
-              wifiSwitch={wifiSwitch}
-              wifiMenuSwitch={wifiMenuSwitch}
-              dark={dark}
-            />
-          )}
+                clickHandler={controlCenterSwitch}
+                value={showControlCenter}
+                Icon={<ArrowLeftRight size={16} color={dark ? '#fff' : '#000'} />}
+              ></TopbarItem>
+              {showControlCenter && (
+                <ControlCenter
+                  dark={dark}
+                  setDark={setDark}
+                  brightness={brightness}
+                  setBrightness={setBrightness}
+                  sound={sound}
+                  setSound={setSound}
+                  controlCenterSwitch={controlCenterSwitch}
+                />
+              )}
+            </div>
+
+            <div className={dark ? 'text-white ' : ''}>
+              {dayjs(date).format('MMMD ddd  HH:mm')}
+            </div>
+          </div>
         </div>
 
-        <TopbarItem
-          Icon={<Search size={16} color={dark ? '#fff' : '#000'} />}
-        ></TopbarItem>
-        <TopbarItem
-          Icon={<MoreHorizontal size={16} color={dark ? '#fff' : '#000'} />}
-        ></TopbarItem>
-        <TopbarItem
-          Icon={<BatteryCharging size={16} color={dark ? '#fff' : '#000'} />}
-        ></TopbarItem>
-        <div className="relative">
-          <TopbarItem
-            clickHandler={controlCenterSwitch}
-            value={showControlCenter}
-            Icon={<ArrowLeftRight size={16} color={dark ? '#fff' : '#000'} />}
-          ></TopbarItem>
-          {showControlCenter && (
-            <ControlCenter
-              dark={dark}
-              setDark={setDark}
-              brightness={brightness}
-              setBrightness={setBrightness}
-              sound={sound}
-              setSound={setSound}
-              controlCenterSwitch={controlCenterSwitch}
-            />
-          )}
-        </div>
+      }
+    </>
 
-        <div className={dark ? 'text-white ' : ''}>
-          {dayjs(date).format('MMMD ddd  HH:mm')}
-        </div>
-      </div>
-    </div>
   )
 }
 
