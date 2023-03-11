2b4c5b28a58900f6adf94f47ffac9970832cf469
ljq0226 <1035821043@qq.com>
2023-03-11 16:45:01 +0800

    fix: eslint auto fix probleam

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common'
-import type { AppService } from './app.service'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
