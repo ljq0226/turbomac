8a257de32b704c22ffff73f88f4255a0bcbb5d95
ljq0226 <1035821043@qq.com>
2023-03-14 14:56:47 +0800

    chore: setting: pipe\filter\interceptor

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
+import { HttpExceptionFilter } from './common/filters/http-exception.fillter';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import {logger} from './common/middleware/logger.middleware'
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalFilters(new HttpExceptionFilter()); //应用过滤器
+  app.useGlobalInterceptors(new TransformInterceptor()); //应用拦截器
+  app.useGlobalPipes(new ValidationPipe()); //应用管道
+  app.use(logger); //应用日志管理
+
   app.enableCors()
-  await app.listen(8080);
+  await app.listen(8080)
 }
-bootstrap();
+bootstrap()
