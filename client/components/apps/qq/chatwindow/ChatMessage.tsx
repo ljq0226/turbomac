1824f9b640ffb2c17a1e78ad70b14bcc6b88add1
ljq0226 <1035821043@qq.com>
2023-03-15 14:09:37 +0800

    fix: package

--- client/components/apps/qq/chatwindow/ChatMessage.tsx
+++ client/components/apps/qq/chatwindow/ChatMessage.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
 interface Props {
