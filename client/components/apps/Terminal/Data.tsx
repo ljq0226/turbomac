cfc85fe5b738e3f222286ebfe97a4c40bdcc949d
ljq0226 <1035821043@qq.com>
2023-03-19 14:24:32 +0800

    feat:add cd and ls

--- client/components/apps/Terminal/Data.tsx
+++ client/components/apps/Terminal/Data.tsx
@@ -1,42 +1,55 @@
-interface TerminalData {
+export interface TerminalData {
   id: string
   title: string
-  type: 'folder' | 'file'
+  type: string
   content?: JSX.Element | string
-  children?: TerminalData[]
+  children: TerminalData[]
 }
 
 // Define the folder structure
-export const FolderStructure: TerminalData[] = [
-  {
-    id: 'desktop',
-    title: 'Desktop',
-    type: 'folder',
-    children: [
-      {
-        id: 'test1',
-        title: 'ReactDemo',
-        type: 'folder',
-        children: [],
-      },
-      {
-        id: 'test1',
-        title: 'NextDemo',
-        type: 'folder',
-        children: [],
-      },
-    ],
-  },
-  {
-    id: 'downloads',
-    title: 'Downloads',
-    type: 'folder',
-    children: [],
-  },
-  {
-    id: 'documents',
-    title: 'Documents',
-    type: 'folder',
-    children: [],
-  },
-]
+export const FolderStructure: TerminalData = {
+  id: '',
+  title: '',
+  type: 'root',
+  children: [
+    {
+      id: 'desktop',
+      title: 'Desktop',
+      type: 'folder',
+      children: [
+        {
+          id: 'test1',
+          title: 'ReactDemo',
+          type: 'folder',
+          children: [
+            {
+              id: 'test3',
+              title: 'test3',
+              type: 'folder',
+              children: [],
+            },
+          ],
+        },
+        {
+          id: 'test2',
+          title: 'NextDemo',
+          type: 'folder',
+          children: [],
+        },
+      ],
+    },
+    {
+      id: 'downloads',
+      title: 'Downloads',
+      type: 'folder',
+      children: [],
+    },
+    {
+      id: 'documents',
+      title: 'Documents',
+      type: 'folder',
+      children: [],
+    },
+  ],
+
+}
--- client/components/apps/Terminal/Terminal.tsx
+++ client/components/apps/Terminal/Terminal.tsx
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react'
 import { useAppsStore, useTerminalStore } from 'store'
 import { shallow } from 'zustand/shallow'
 import { motion } from 'framer-motion'
+import { useLocalStorageState } from 'ahooks'
 import { CommandNotFound, Help, Row } from './Util'
 import { FolderStructure } from './Data'
+import type { TerminalData } from './Data'
 import { generateRandomString } from '@/lib/utils'
-
 interface CommandList {
-  [key: string]: { (): void } | {
-    (arg: string): void
-  }
+  [key: string]:
+  { (): void } | { (arg: string | TerminalData): void }
 }
 
 const Terminal: React.FC = () => {
@@ -20,6 +20,8 @@ const Terminal: React.FC = () => {
   const [changeCount, setChangeCount] = useState<number>(0)
   const [commandHistory, setCommandHistory] = useState<string[]>([])
   const [currentDirectory, setCurrentDirectory] = useState<string>('')
+  const [targetFolder, setTargetFolder] = useState<TerminalData>(FolderStructure)
+  const [lsItems, setLsItmes] = useLocalStorageState('LS_Items', { defaultValue: targetFolder.children.map(item => item.title).join('   ') })
   const [content, setContent] = useState<JSX.Element[]>(
     [<Row
       key={generateRandomString()}
@@ -45,52 +47,80 @@ const Terminal: React.FC = () => {
   useEffect(() => {
     const span = document.querySelector(`#terminal-currentDirectory-${commandHistory.length}`) as HTMLSpanElement
     span.innerHTML = currentDirectory
+    localStorage.setItem('Terminal-CurrentDirectory', currentDirectory)
   }, [currentDirectory, commandHistory.length])
+
+  useEffect(() => {
+    setCurrentDirectory(`${currentDirectory + targetFolder?.title}${targetFolder?.type === 'folder' ? '/' : ''}`)
+    const items = targetFolder.children.map(item => item.title).join('   ')
+    setLsItmes(items)
+  }, [targetFolder])
 
   const generateRow = (row: JSX.Element) => {
     setContent(s => [...s, row])
   }
+
   const clear = () => {
     setContent([])
     const input = document.querySelector('#terminal-input-0') as HTMLInputElement
     input.value = ''
   }
 
-  const open = (arg: string) => {
+  const open = (arg = '') => {
     generateRow(<div key={generateRandomString()}>Opening {arg}...</div>)
     openApp(arg)
   }
-  const close = (arg: string) => {
+  const close = (arg = '') => {
     closeApp(arg)
     generateRow(<div key={generateRandomString()}>Closed {arg}...</div>)
   }
-  const cd = (arg: string) => {
-    if (arg === '..') {
-      if (currentDirectory !== '')
+  const dfs = (node: TerminalData, args: string[]) => {
+    if (!node)
+      return 0
+    for (let i = 0; i < node.children.length; i++) {
+      if (!args.includes(node.children[i].title.toLowerCase())) {
+        dfs(node.children[i], args)
+      }
+      else {
+        setTargetFolder(node.children[i])
+        return 1
+      }
+    }
+  }
+  const cd = (arg = '') => {
+    const paths = (localStorage.getItem('Terminal-CurrentDirectory') as string)
+    const args = [arg, arg.toUpperCase(), arg.toLowerCase(), arg.charAt(0).toUpperCase() + arg.slice(1)]
+    if (arg === '..' || !arg) {
+      if (!paths) {
         setCurrentDirectory('')
+      }
+      else {
+        // to resolve when the current directory in first layer
+        if (paths.split('/').length <= 2) {
+          setCurrentDirectory('')
+          setTargetFolder(FolderStructure)
+        }
+        else {
+          const curPath = paths.split('/').slice(0, -2).join('/')
+          const newArg = curPath.split('/').pop() as string
+          const newArgs = [newArg, newArg.toUpperCase(), newArg.toLowerCase(), newArg.charAt(0).toUpperCase() + newArg.slice(1)]
+          dfs(FolderStructure, newArgs)
+          setTimeout(() => {
+            setCurrentDirectory(`${curPath}/`)
+          }, 10)
+        }
+      }
     }
     else {
-      const targetFolder = FolderStructure.find(folder => folder.title === arg || folder.title === (arg.charAt(0).toUpperCase() + arg.slice(1)))
-      if (targetFolder)
-        setCurrentDirectory(`${currentDirectory + targetFolder.title}/`)
-
-      else generateRow(<div key={generateRandomString()}>Directory not found: {arg}</div>)
+      dfs(targetFolder, args)
+      const target = JSON.parse(localStorage.getItem('LS_Items') as string).split(' ').some(
+        (item: string) => item === arg || item.toLowerCase() === arg || item.toLowerCase() === arg.toLowerCase(),
+      )
+      !target && generateRow(<div key={generateRandomString()}>Directory of File not found: {arg}</div>)
     }
   }
   const ls = () => {
-    if (currentDirectory === '') {
-      const items = FolderStructure.map(folder => folder.title).join(' ')
-      generateRow(<div key={generateRandomString()}>{items}</div>)
-      return
-    }
-    const currentFolder = FolderStructure.find(folder => folder.title === currentDirectory)
-    if (currentFolder && currentFolder.children) {
-      const items = currentFolder.children.map(item => item.title).join(' ')
-      generateRow(<div key={generateRandomString()}>{items}</div>)
-    }
-    else {
-      generateRow(<div key={generateRandomString()}>No files or folders found in the current directory.</div>)
-    }
+    generateRow(<div key={generateRandomString()}>{JSON.parse(localStorage.getItem('LS_Items') as string)}</div>)
   }
 
   function handleArrowUp() {
@@ -100,6 +130,7 @@ const Terminal: React.FC = () => {
   function handleArrowDown() {
     setChangeCount(prev => Math.min(prev + 1, 0))
   }
+
   const matchCommand = (inputValue: string): string | null => {
     const matchedCommands = commandHistory.filter(command => command.startsWith(inputValue))
     return matchedCommands.length > 0 ? matchedCommands[matchedCommands.length - 1] : null
@@ -161,6 +192,7 @@ const Terminal: React.FC = () => {
       style={{ fontFamily: 'Menlo, monospace', fontSize: '14px' }}
     >
       <div className="h-6 rounded-lg"></div>
+      <div>{currentDirectory}</div>
       <div className="flex flex-col w-full h-[400px] overflow-y-scroll mb-2 chatlist_">
         <div className='flex-1 w-full'>
           {...content}
